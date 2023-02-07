5a6845b2cd4755a3671ba4e76223b42709059304
Preslav Getov <preslav.getov@yara.com>
2023-02-07 14:42:23 +0200

    func styles are done - only func expressions are valid

--- intro.js
+++ intro.js
@@ -1,4 +1,7 @@
-function addDetails(){
-    return 'these are all the details';
-}
-
+// function addDetails(){
+//     return 'these are all the details';
+// }
+const addDetails = ()=>{
+    return 'these are details';
+};
+console.log(1 === '1');
