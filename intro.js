820ac330c2b73e465e526ffa29801b5e50d09ecf
Preslav Getov <preslav.getov@yara.com>
2023-02-07 14:48:48 +0200

    some restructuring done for the project

--- intro.js
+++
@@ -1,7 +0,0 @@
-// function addDetails(){
-//     return 'these are all the details';
-// }
-const addDetails = ()=>{
-    return 'these are details';
-};
-console.log(1 === '1');
---
+++ src/intro.js
@@ -0,0 +1,8 @@
+// function addDetails(){
+//     return 'these are all the details';
+// }
+const addDetails = () => {
+  return "these are details";
+};
+
+console.log(1 === "1");
